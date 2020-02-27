4d26db5c7cbc1ce018349deda7eb468b40e61b20
Cookie Vang <vang2660@morris.umn.edu>
2020-02-27 13:55:13 -0600

    Add add-todo files
    Create component for adding new todos to database

---
+++ client/src/app/todos/add-todo.component.ts
@@ -0,0 +1,102 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { Todo } from './todo';
+import { TodoService } from './todo.service';
+
+@Component({
+  selector: 'app-add-todo',
+  templateUrl: './add-todo.component.html',
+  styleUrls: ['./add-todo.component.scss']
+})
+export class AddTodoComponent implements OnInit {
+
+  addTodoForm: FormGroup;
+
+  todo: Todo;
+
+  constructor(private fb: FormBuilder, private todoService: TodoService, private snackBar: MatSnackBar, private router: Router) {
+  }
+
+  // not sure if this name is magical and making it be found or if I'm missing something,
+  // but this is where the red text that shows up (when there is invalid input) comes from
+  add_todo_validation_messages = {
+    owner: [
+      {type: 'required', message: 'Owner is required'},
+      {type: 'minlength', message: 'Owner must be at least 2 characters long'},
+      {type: 'maxlength', message: 'Owner cannot be more than 50 characters long'},
+      {type: 'pattern', message: 'Owner must contain only numbers and letters'},
+      {type: 'existingOwner', message: 'Owner has already been taken'}
+    ],
+
+    category: [
+      {type: 'required', message: 'Category is required'}
+    ],
+
+    status: [
+      { type: 'required', message: 'Status is required' },
+      { type: 'pattern', message: 'Status must be True or False' },
+    ]
+  };
+
+  createForms() {
+
+    // add todo form validations
+    this.addTodoForm = this.fb.group({
+      // We allow alphanumeric input and limit the length for name.
+      owner: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(2),
+        // In the real world you'd want to be very careful about having
+        // an upper limit like this because people can sometimes have
+        // very long names. This demonstrates that it's possible, though,
+        // to have maximum length limits.
+        Validators.maxLength(50),
+        Validators.pattern('^[A-Za-z0-9\\s]+[A-Za-z0-9\\s]+$(\\.0-9+)?'),
+        (fc) => {
+          if (fc.value.toLowerCase() === 'abc123' || fc.value.toLowerCase() === '123abc') {
+            return ({existingName: true});
+          } else {
+            return null;
+          }
+        },
+      ])),
+
+      // We don't need a special validator just for our app here, but there is a default one for category.
+      // We will require the category, though.
+      category: new FormControl('', Validators.compose([
+        Validators.required,
+      ])),
+
+      // We don't care much about what is in the body field, so we just add it here as part of the form
+      // without any particular validation.
+      body: new FormControl(),
+
+      status: new FormControl('viewer', Validators.compose([
+        Validators.required,
+        Validators.pattern('^(true|false)$'),
+      ])),
+    });
+
+  }
+
+  ngOnInit() {
+    this.createForms();
+  }
+
+
+  submitForm() {
+    this.todoService.addTodo(this.addTodoForm.value).subscribe(newID => {
+      this.snackBar.open('Added Todo ' + this.addTodoForm.value.owner, null, {
+        duration: 2000,
+      });
+      this.router.navigate(['/todos/', newID]);
+    }, err => {
+      this.snackBar.open('Failed to add the todo', null, {
+        duration: 2000,
+      });
+    });
+  }
+
+}
