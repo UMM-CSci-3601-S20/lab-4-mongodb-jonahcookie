dd6c733bc94ba0140064c9618eb16038f3ee76d3
Cookie Vang <vang2660@morris.umn.edu>
2020-02-27 14:41:00 -0600

    Add todo-list component files
    Create files that adds the list component for the todos

---
+++ client/src/app/todos/todo-list-component.ts
@@ -0,0 +1,74 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Todo, StatusType } from './todo';
+import { TodoService } from './todo.service';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-todo-list-component',
+  templateUrl: 'todo-list.component.html',
+  styleUrls: ['./todo-list.component.scss'],
+  providers: []
+})
+
+export class TodoListComponent implements OnInit, OnDestroy  {
+  // These are public so that tests can reference them (.spec.ts)
+  public serverFilteredTodos: Todo[];
+  public filteredTodos: Todo[];
+
+  public todoOwner: string;
+  public todoCategory: string;
+  public todoStatus: StatusType;
+  public todoBody: string;
+  public viewType: 'card' | 'list' = 'card';
+  getTodosSub: Subscription;
+
+
+  // Inject the TodoService into this component.
+  // That's what happens in the following constructor.
+  //
+  // We can call upon the service for interacting
+  // with the server.
+
+  constructor(private todoService: TodoService) {
+
+  }
+
+  getTodosFromServer(): void {
+    this.unsub();
+    this.getTodosSub = this.todoService.getTodos({
+      status: this.todoStatus,
+      body: this.todoBody,
+      category: this.todoCategory,
+
+
+    }).subscribe(returnedTodos => {
+      this.serverFilteredTodos = returnedTodos;
+      this.updateFilter();
+    }, err => {
+      console.log(err);
+    });
+  }
+
+  public updateFilter(): void {
+    this.filteredTodos = this.todoService.filterTodos(
+      this.serverFilteredTodos, { owner: this.todoOwner, category: this.todoCategory });
+  }
+
+  /**
+   * Starts an asynchronous operation to update the todos list
+   *
+   */
+  ngOnInit(): void {
+    this.getTodosFromServer();
+  }
+
+  ngOnDestroy(): void {
+    this.unsub();
+  }
+
+  unsub(): void {
+    if (this.getTodosSub) {
+      this.getTodosSub.unsubscribe();
+    }
+  }
+}
